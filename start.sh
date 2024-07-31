0fbc94eca4f5caf7e70c916500d67c4e60ec0e57
7zism3 <7zism3@gmail.com>
2024-07-31 13:41:35 +0700

    #setup - edit docker-compose.yml
    edit

--- start.sh
+++ start.sh
@@ -8,6 +8,13 @@ fi
 # Chạy Docker Compose cho các file
 docker-compose -f postgres/docker-compose.yml up -d
 
+# Kiểm tra tình trạng của PGDB trước khi start các services khác
+until docker run --rm --network sevenhallo postgres:latest pg_isready -h postgres -U postgres; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 2
+done
+echo "PostgreSQL is ready!"
+
 docker-compose -f keycloak/docker-compose.yml up -d
 docker-compose -f nexus/docker-compose.yml up -d
 docker-compose -f redis/docker-compose.yml up -d
